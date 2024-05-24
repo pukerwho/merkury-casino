e9ac44bccdbecef7973c43f612cee3a1ee81ccb0
Puker Who <nawwesele@gmail.com>
2024-05-24 16:52:25 +0300

    edit

--- footer.php
+++ footer.php
@@ -5,10 +5,10 @@
     <div class="text-center mb-4">
       <?php _e("Участие в азартных играх может вызвать игровую зависимость. Соблюдайте правила (принципы) ответственной игры", "treba-wp"); ?>
     </div>
-    <div class="text-center text-lg mb-2">
+    <div class="text-center text-lg mb-4">
       <a href="<?php echo get_home_url(); ?>" class="bg-blue-500 rounded px-4 py-2 text-xl text-white font-extrabold">Merkury</a>
     </div>
-    <div>
+    <div class="flex justify-center">
       <a href="https://sviato.top/"><img src="https://merkury.com.ua/wp-content/uploads/2024/05/sviato.jpeg" alt="Sviato.top" width="25"></a>
     </div>
   </div>
